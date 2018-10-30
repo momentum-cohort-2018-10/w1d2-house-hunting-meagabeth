7b2119cdc4ff5087a76839a1cfd7982fab19141c
Mo Glatz <meagabeth@icloud.com>
2018-10-30 14:48:57 -0400

    House Hunting Lab

---
+++ house_hunting.py
@@ -0,0 +1,6 @@
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
+
+# portion_down_payment = total_cost*.25
+# current_savings = current_savings + current_savings*r/12
