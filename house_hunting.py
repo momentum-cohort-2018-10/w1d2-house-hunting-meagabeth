fa481d840162eeb32bd0ab37badcb7affb249fb7
Mo Glatz <meagabeth@icloud.com>
2018-10-30 17:21:42 -0400

    Revised lab work

--- house_hunting.py
+++ house_hunting.py
@@ -4,3 +4,12 @@ total_cost = float(input("Enter the cost of your dream home: "))
 
 # portion_down_payment = total_cost*.25
 # current_savings = current_savings + current_savings*r/12
+num_of_months = 1
+portion_down_payment = total_cost*.25
+current_savings = 0
+r = .04
+
+while current_savings < portion_down_payment:
+    num_of_months += 1
+    current_savings = current_savings + current_savings*r/12 + (annual_salary/12*portion_saved)
+print("Number of months: " + str(num_of_months))
